08c44e2af893d2e35b0ff1680483290e784d582d
Hemantgarg06 <99550853+Hemantgarg06@users.noreply.github.com>
2023-07-10 19:38:06 +0530

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function init(){
-    const slides = document.querySelectorAll(".slide");   //slides are three dots
-    const pages = document.querySelectorAll(".page");     //all of our pages 
-    const backgrounds = [`radial-gradient(#2B3760, #0B1023)`,`radial-gradient(#4E3022, #161616)`,`radial-gradient(#4E4342,#161616)`];   //just for bg colors
+    const slides = document.querySelectorAll(".slide");   
+    const pages = document.querySelectorAll(".page");     
+    const backgrounds = [`radial-gradient(#2B3760, #0B1023)`,`radial-gradient(#4E3022, #161616)`,`radial-gradient(#4E4342,#161616)`];   
     
     //Tracker
     let current = 0;
     let scrollSlide = 0;
 
-    slides.forEach((slide,index) => {    //to manage dots
+    slides.forEach((slide,index) => {    
         slide.addEventListener('click',function(){
             changeDots(this);
-            nextSlide(index);   //represents the no. we are clicking on
+            nextSlide(index);   
             scrollSlide = index;
         });
     });
@@ -19,18 +19,18 @@ function init(){
         slides.forEach(slide => {
             slide.classList.remove("active");
         });
-        dot.classList.add('active')   //when we click on dots we add a class of active on dots
+        dot.classList.add('active')   
     }
 
     function nextSlide(pageNumber){
-        const nextPage = pages[pageNumber];   //we get the next page we are click on
+        const nextPage = pages[pageNumber];   
         const currentPage = pages[current];
         const nextLeft = nextPage.querySelector(".hero .model-left");
         const nextRight = nextPage.querySelector(".hero .model-right");
         const currentLeft = currentPage.querySelector(".hero .model-left");
         const currentRight = currentPage.querySelector(".hero .model-right");
-        const nextText = nextPage.querySelector(".details");      //text that are coming
-        const portfolio = document.querySelector(".portfolio");   //whole page
+        const nextText = nextPage.querySelector(".details");      
+        const portfolio = document.querySelector(".portfolio");   
 
         const tl = new TimelineMax({
             onStart: function(){
@@ -46,13 +46,13 @@ function init(){
         });
 
         tl.fromTo(currentLeft, 0.3, {y:"-10%"},{y:"-100%"})
-        .fromTo(currentRight, 0.3, {y:'10%'},{y:'-100%'}, '-=0.2')    //with -0.2 it will start faster i.e. after 0.1 ms from currentLeft
-        .to(portfolio, 0.3, {backgroundImage:backgrounds[pageNumber]})    //to change bg color
-        .fromTo(currentPage,0.3,{opacity:1,pointerEvents:'all'},{opacity:0,pointerEvents:'none'})   //current page we are on and fade it out
-        .fromTo(nextPage,0.3,{opacity:0,pointerEvents:'none'},{opacity:1,pointerEvents:'all'},"-=0.6")     //next page back
+        .fromTo(currentRight, 0.3, {y:'10%'},{y:'-100%'}, '-=0.2')    
+        .to(portfolio, 0.3, {backgroundImage:backgrounds[pageNumber]})    
+        .fromTo(currentPage,0.3,{opacity:1,pointerEvents:'all'},{opacity:0,pointerEvents:'none'})   
+        .fromTo(nextPage,0.3,{opacity:0,pointerEvents:'none'},{opacity:1,pointerEvents:'all'},"-=0.6")     
         
-        .fromTo(nextLeft,0.3,{y:'-100%'}, {y:'-10%'},'-=0.6')         // images that are coming in
-        .fromTo(nextRight,0.3,{y:'-100%'}, {y:'10%'},'-=0.8')         // images that are coming in
+        .fromTo(nextLeft,0.3,{y:'-100%'}, {y:'-10%'},'-=0.6')         
+        .fromTo(nextRight,0.3,{y:'-100%'}, {y:'10%'},'-=0.8')         
         .fromTo(nextText,0.3,{opacity:0,y:0},{opacity:1,y:0})
         .set(nextLeft, {clearProps:"all"})
         .set(nextRight, {clearProps:"all"});
@@ -64,7 +64,7 @@ function init(){
     document.addEventListener('touchmove',throttle(scrollChange,1500));
 
     function switchDots(dotNumber){
-        const activeDot = document.querySelectorAll(".slide")[dotNumber];     //to change slides with dots
+        const activeDot = document.querySelectorAll(".slide")[dotNumber];     
         slides.forEach(slide=>{
             slide.classList.remove("active");
         });
@@ -79,10 +79,10 @@ function init(){
             scrollSlide -= 1;
         }
         
-        if(scrollSlide > 2){    //if slides more then 2 times then back to 0
+        if(scrollSlide > 2){    
             scrollSlide = 0;
         }
-        if(scrollSlide <0){    //if slides less then 0 then go to 2
+        if(scrollSlide <0){    
             scrollSlide = 2;
         }
         switchDots(scrollSlide);
@@ -105,8 +105,8 @@ function init(){
     .fromTo(logo,0.2,{color:'white'},{color:'black'}, '-=1')
     .fromTo(hamburgerLines,0.2,{stroke:'white'},{stroke:'black'},'-=1');
 
-    hamburger.addEventListener('click',()=>{           // this is for three lines 
-        tl.reversed() ? tl.play() : tl.reverse();     // it checks the reverse the play the animation and reverse when we click
+    hamburger.addEventListener('click',()=>{           
+        tl.reversed() ? tl.play() : tl.reverse();     
     });
  }
 
